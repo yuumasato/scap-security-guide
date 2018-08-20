f1c5a8ecc0b7bc452ead593acb8dad272d1121c2
Watson Sato <wsato@redhat.com>
2018-08-20 08:23:42 +0200

    Prototype of a content updater
    Currently blocked by the fact that a rule.yml is not a yml file, it
    contains jinja macros that prevents load, edit, dump. The load process
    raises an exception

---
+++ utils/content-updater.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+
+import ssg.build_yaml
+import ssg.rules
+import ssg.rule_dir_stats as rds
+
+def apply_query(rule_obj, query):
+    # query has form of: products.contains:rhel7
+    # query has form of: oval.products.is:rhel7
+    full_query_field, query_value = query.split(':')
+    try:
+        language, field, comparator = full_query_field.split('.')
+        field_value = rule_obj[language][field]
+    except ValueError:
+        field, comparator = full_query_field.split('.')
+        field_value = rule_obj[field]
+
+    # TODO: Are all field_values lists?
+    # This should either be optimized for lists, or made generic for string and dictionary
+    if comparator == "is":
+        if isinstance(field_value, list):
+            if len(field_value)==1 and query_value in field_value:
+                return True
+        elif isinstance(field_value, str):
+            if query_value == field_value:
+                return True
+    elif comparator == "contains":
+        if isinstance(field_value, list):
+            if query_value in field_value:
+                return True
+        elif isinstance(field_value, str):
+            if field_value.contains(query_value):
+                return True
+    else:
+        # TODO: Raise error, unknown comparator
+        return False
+
+    return False
+
+
+def update_contents(rule_obj, remove_exp, add_exp):
+    if remove_exp:
+        full_remove_field, remove_value = remove_exp.split(':')
+
+        try:
+            language, field, comparator = full_remove_field.split('.')
+            field_value = rule_obj[language][field]
+        except ValueError:
+            field, comparator = full_remove_field.split('.')
+            field_value = rule_obj[field]
+
+
+    if add_exp:
+        full_add_field, value_to_add = add_exp.split(':')
+
+        try:
+            language, field = full_add_field.split('.')
+            field_value = rule_obj[language][field]
+        except ValueError:
+            field = full_add_field
+            field_value = rule_obj[field]
+
+        if isinstance(field_value, list):
+            if value_to_add not in field_value:
+                field_value.append(value_to_add)
+            
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, action="store", default="build/rule_dirs.json",
+                        help="File to read json output of rule_dir_json from (defaults to build/rule_dirs.json)")
+    parser.add_argument("-q", "--query", type=str, action="store", default=None,
+                        help="Query witch rules to update.")
+    parser.add_argument("-r", "--remove", type=str, action="store", default=None,
+                        help="Field to remove from rules that match the query.")
+    parser.add_argument("-a", "--add", type=str, action="store", default=None,
+                        help="Field to add into rules that match the query.")
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    json_file = open(args.input, 'r')
+    known_rules = json.load(json_file)
+
+    for rule_id in known_rules:
+        if args.query is None or apply_query(known_rules[rule_id], args.query):
+            if (args.remove or args.add):
+                update_contents(known_rules[rule_id], args.remove, args.add)
+
+                rule_file = ssg.rules.get_rule_dir_yaml(known_rules[rule_id]['dir'])
+                print(rule_file)
+                rule_yaml = ssg.build_yaml.Rule.from_yaml(rule_file)
+                print(rule_yaml)
+
+                #print(rule_id +": "+ str(known_rules[rule_id]["products"]))
+            else:
+                # Just print the rule
+                #print(rule_id +": "+ str(known_rules[rule_id]["products"]))
+                pass
+
+    # regenerate rule_dirs.json
+
+if __name__ == "__main__":
+    main()
