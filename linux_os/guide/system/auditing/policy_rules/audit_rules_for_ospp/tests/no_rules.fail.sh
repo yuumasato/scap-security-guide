f76ef9745a1120a3778b9f4503b374cfd29d9d2a
Watson Sato <wsato@redhat.com>
2021-08-09 11:16:46 +0200

    WIP fix test scenarios

--- linux_os/guide/system/auditing/policy_rules/audit_rules_for_ospp/tests/no_rules.fail.sh
+++ linux_os/guide/system/auditing/policy_rules/audit_rules_for_ospp/tests/no_rules.fail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # These tests are for systems with 30-ospp-v42.rules file installed
-if [ ! -f /usr/share/doc/audit*/rules/30-ospp-v42.rules ]; then
+if [ ! -f /usr/share/audit/sample-rules/30-ospp-v42.rules ]; then
     exit 1
 fi
 rm -rf /etc/audit/rules.d/*
